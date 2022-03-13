7c9e4cf336ec1f52e9ef0d136e0d16abcd61ca99
JU MYUNGHOON <myunghoonju@gmail.com>
2022-03-14 02:57:54 +0900

    https://www.baeldung.com/java-netty-http-server

--- src/main/java/org/hello/MyspringApplication.java
+++ src/main/java/org/hello/MyspringApplication.java
@@ -2,6 +2,7 @@ package org.hello;
 
 import lombok.RequiredArgsConstructor;
 import org.hello.netty.client.NettyClient;
+import org.hello.netty.server.HttpServer;
 import org.hello.netty.server.NettyServer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,11 +14,11 @@ public class MyspringApplication implements CommandLineRunner {
 
     private final NettyServer server;
     private final NettyClient client;
+    private final HttpServer httpServer;
 
     public static void main(String[] args) {
         SpringApplication.run(MyspringApplication.class, args);
     }
-
     @Override
     public void run(String... args) throws Exception {
         Thread sthread = new Thread(server);
@@ -28,4 +29,13 @@ public class MyspringApplication implements CommandLineRunner {
         cthread.join();
 
     }
+
+/*
+    @Override
+    public void run(String... args) throws Exception {
+        Thread sthread = new Thread(httpServer);
+        sthread.start();
+        sthread.join();
+    }
+ */
 }
---
+++ src/main/java/org/hello/netty/server/HttpServer.java
@@ -0,0 +1,64 @@
+package org.hello.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HttpServer implements Runnable {
+
+    private static Integer port = 8090;
+
+    public static void main(String[] args) throws Exception {
+        new NettyServer().run();
+    }
+
+    @Override
+    public void run() {
+        start();
+    }
+
+    public void start() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new HttpRequestDecoder());
+                            pipeline.addLast(new HttpResponseEncoder());
+                            pipeline.addLast(new CustomHttpServerHandler());
+                        }
+                    }).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
